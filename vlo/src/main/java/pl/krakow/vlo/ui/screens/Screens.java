1e71f1f4507663d2248dcb68fbbba776753975c4
m4tx <m4tx@m4tx.pl>
2014-05-02 17:07:31 +0200

    Add more real app modules stubs

--- vlo/src/main/java/pl/krakow/vlo/ui/screens/Screens.java
+++ vlo/src/main/java/pl/krakow/vlo/ui/screens/Screens.java
@@ -1,5 +1,11 @@
 package pl.krakow.vlo.ui.screens;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 import pl.krakow.vlo.R;
@@ -11,18 +17,37 @@ public abstract class Screens {
     private static ArrayList<Screen> screens = new ArrayList<>();
 
     static {
-        screens.add(new Header(R.string.app_name));
         screens.add(new InternetConnectionScreen());
-        screens.add(new InternetConnectionScreen());
-        screens.add(new Header(R.string.app_name));
-        screens.add(new InternetConnectionScreen());
-        screens.add(new InternetConnectionScreen());
-        screens.add(new Header(R.string.app_name));
-        screens.add(new InternetConnectionScreen());
-        screens.add(new InternetConnectionScreen());
+        screens.add(new Header(R.string.screen_section_lessons));
+        screens.add(new TimetableScreen());
     }
 
     public static ArrayList<Screen> getScreens() {
         return screens;
+    }
+
+    public static View create1LineListItem(LayoutInflater inflater, ViewGroup viewGroup,
+                                           int textResId, int iconResId) {
+        return create1LineListItem(inflater, viewGroup, R.layout.list_item_1, textResId,
+                iconResId);
+    }
+
+    private static View create1LineListItem(LayoutInflater inflater, ViewGroup viewGroup,
+                                            int layoutResId, int textResId, int iconResId) {
+        View view = inflater.inflate(layoutResId, viewGroup, false);
+        TextView tv = (TextView) view.findViewById(android.R.id.text1);
+        tv.setText(textResId);
+        ImageView iv = (ImageView) view.findViewById(android.R.id.icon);
+        iv.setImageResource(iconResId);
+        return view;
+    }
+
+    public static View create2LineListItem(LayoutInflater inflater, ViewGroup viewGroup,
+                                           int text1ResId, int text2ResId, int iconResId) {
+        View view = create1LineListItem(inflater, viewGroup, R.layout.list_item_2,
+                text1ResId, iconResId);
+        TextView tv = (TextView) view.findViewById(android.R.id.text2);
+        tv.setText(text2ResId);
+        return view;
     }
 }
--- vlo/src/main/java/pl/krakow/vlo/ui/screens/TimetableScreen.java
+++ vlo/src/main/java/pl/krakow/vlo/ui/screens/TimetableScreen.java
@@ -12,16 +12,16 @@ import pl.krakow.vlo.R;
 /**
  * Created by m4tx3 on 5/2/14.
  */
-public class InternetConnectionScreen implements Screen {
+public class TimetableScreen implements Screen {
     @Override
     public int getNameResId() {
-        return R.string.screen_connection_title;
+        return R.string.screen_timetable_title;
     }
 
     @Override
     public View getListItemView(LayoutInflater inflater, ViewGroup viewGroup) {
-        return Screens.create2LineListItem(inflater, viewGroup, getNameResId(), getNameResId(),
-                R.drawable.ic_internet_connection);
+        return Screens.create1LineListItem(inflater, viewGroup, getNameResId(),
+                R.drawable.ic_timetable);
     }
 
     @Override
