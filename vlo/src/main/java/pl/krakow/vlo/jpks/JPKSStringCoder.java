68f14732e38546bc8414edf39efe3493dfbb6683
m4tx <m4tx@m4tx.pl>
2014-05-04 12:37:53 +0200

    Add documentation for JPKSStringCoder class

--- vlo/src/main/java/pl/krakow/vlo/jpks/JPKSStringCoder.java
+++ vlo/src/main/java/pl/krakow/vlo/jpks/JPKSStringCoder.java
@@ -6,7 +6,8 @@ import static java.util.AbstractMap.SimpleEntry;
 import static java.util.Map.Entry;
 
 /**
- * Created by m4tx3 on 5/4/14.
+ * The class that is used to encode and decode Strings in "JPKS encoding". It is a simple encoding
+ * to allow for using polish diacritics and sending them as ASCII text.
  */
 public abstract class JPKSStringCoder {
     private static final ArrayList<Entry<String, String>> CODER_MAP = new ArrayList<>();
@@ -32,6 +33,12 @@ public abstract class JPKSStringCoder {
         CODER_MAP.add(new SimpleEntry<>("Ż", "%Z"));
     }
 
+    /**
+     * Encodes a string, producing ready to send sequence of characters in JPKS encoding.
+     *
+     * @param string the string to encode
+     * @return the string to send
+     */
     public static String encodeString(String string) {
         for (Entry<String, String> entry : CODER_MAP) {
             string = string.replaceAll(entry.getKey(), entry.getValue());
@@ -39,6 +46,12 @@ public abstract class JPKSStringCoder {
         return string;
     }
 
+    /**
+     * Decodes a JPKS-encoded string, producing a regular UTF-8 sequence of characters.
+     *
+     * @param string the string received
+     * @return UTF-8 string
+     */
     public static String decodeString(String string) {
         for (Entry<String, String> entry : CODER_MAP) {
             string = string.replaceAll(entry.getValue(), entry.getKey());
